71d346a830247cac3cde33cb55ed709938ffd85d
Fredy <142295949+fredrickomondiodongo@users.noreply.github.com>
2024-02-19 17:40:20 +0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,1 +1,16 @@
+var  side = document.getElementById("side");
+var openbtn = document.getElementById("openbtn");
+var openbutton = document.getElementById("openbutton")
+
+openbutton.onclick=function(){
+    if(side.style.right== "-250px" ){
+        side.style.right = "0px"
+    } 
+    else if(side.style.right=="0px"){
+        side.style.right = "-250px"
+    }
+    else(side.style.right="0px")
+
+    
+}
 
